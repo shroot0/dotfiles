4758619392f15f860dd8d55578769d85e7190150
Mike Shubin <shroot0@hotmail.com>
2025-09-04 21:21:20 -0400

    roslyn + ocaml

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -3,9 +3,7 @@ return {
 		"nvim-lspconfig",
 	},
 	{
-		"Decodetalkers/csharpls-extended-lsp.nvim",
-		config = function()
-			require("csharpls_extended").buf_read_cmd_bind()
-		end,
+		"seblyng/roslyn.nvim",
+		opts = {},
 	},
 }
--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -4,6 +4,7 @@ return {
 		opts = {
 			registries = {
 				"github:mason-org/mason-registry",
+				"github:Crashdummyy/mason-registry",
 			},
 		},
 	},
@@ -12,7 +13,7 @@ return {
 		opts = {
 			ensure_installed = {
 				"lua_ls",
-				"csharp_ls",
+				"ocamllsp",
 			},
 		},
 		dependencies = {
@@ -25,8 +26,10 @@ return {
 		opts = {
 			ensure_installed = {
 				"stylua",
+				"roslyn",
 				"csharpier",
 				"netcoredbg",
+				"ocamlformat",
 			},
 		},
 		dependencies = {
